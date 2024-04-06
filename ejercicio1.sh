17266a8332cc1684fbfb01a1048dfd958e57a949
dfonsecz <93692529+dfonsecz@users.noreply.github.com>
2024-04-05 20:36:21 -0600

    Update ejercicio1.sh
    Added commentary

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -2,8 +2,8 @@
 
 # Funciones
 
-get_permissions_verbose() {
-    local permissions=$1
+get_permissions() {
+    local permissions=$1 # Configura variable 
 
     # Funcion para interpretar los permisos como read, write, execute o unknown
     interpret_permission() {
@@ -26,7 +26,8 @@ get_permissions_verbose() {
     # Funcion para imprimir los permisos
     print_permission() {
     local specific_permission=$1
-
+    
+    # Imprime las versiones interpretadas de los permisos
     for (( i=0; i<${#specific_permission}; i++ )); do
         permission=${specific_permission:$i:1}
         result="$(interpret_permission "$permission")"
@@ -37,29 +38,32 @@ get_permissions_verbose() {
 }
 
     # Imprimir permisos para cada tipo
+
+    # Imprime caracter 1-3 del string
     user_permissions=${permissions:1:3}
     echo "User permissions:"
     print_permission "$user_permissions"
 
+    # Imprime caracter 4-6 del string
     group_permissions=${permissions:4:3}
     echo "Group permissions:"
     print_permission "$group_permissions"
 
+    # Imprime caracter 7-9 del string
     other_permissions=${permissions:7:3}
     echo "Other user permissions:"
     print_permission "$other_permissions"
 }
 
-# Main
+# Codigo principal
 
 file=$1
 
+# Verificar si el archivo existe o no
 if [ $# -ne 1 ]; then
     echo "Uso: $0 archivo"
     exit 1
 fi
-
-# Verificar si el archivo existe o no
 
 # Ejecutar si el archivo no existe
 if [ ! -e "$file" ]; then
@@ -71,7 +75,8 @@ else
     # Verifica permisos del archivo
     permissions="$(stat -c "%A" "$file")"
 
-    get_permissions_verbose "$permissions"
+    # Llamar a la funcion get_permissions
+    get_permissions "$permissions"
 
     exit 0
 fi
