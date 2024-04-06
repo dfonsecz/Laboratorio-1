3f8c64443fed1d5b41e009ecd333df92d572d198
dfonsecz <93692529+dfonsecz@users.noreply.github.com>
2024-04-05 23:49:29 -0600

    Update ejercicio3.sh
    Added comments

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -2,6 +2,7 @@
 
 # Funciones
 
+# 
 main () {
     get_help
 
@@ -10,6 +11,7 @@ main () {
     check_option $option
 }
 
+# Imprimir menú de ayuda
 get_help () {
     echo " "
     echo "Uso: ejercicio3.sh"
@@ -22,31 +24,26 @@ get_help () {
     echo " "
 }
 
+# Revisar opciones
 check_option () {
     local opt OPTIND OPTARG
+    # Condiciones seteadas para identificar si se señaló un modo o fecha
     m=false
     d=false
 
+    # Toma decisiones dependiendo de si aparencen las opciones -h, -m o -d
     while getopts "hm:d:" opt; do
         case ${opt} in
             h)
-            get_help
+            get_help # Llama a la función de imprimir el menú de ayuda
             ;;
             m)
-            m=true
-            mode="$OPTARG"
+            m=true # Cambia condición a verdadero si se especifica el modo
+            mode="$OPTARG" # Guarda el argumento para la opción -m
             ;;
             d)
-            d=true
-            date="$OPTARG"
-            ;;
-            \?)
-            echo "Invalid option: -$OPTARG" >&2
-            exit 1
-            ;;
-            :)
-            echo "Option -$OPTARG requires an argument." >&2
-            exit 1
+            d=true # Cambia condición a verdadero si se especifica la fecha
+            date="$OPTARG" # Guarda el argumento para la opción -d
             ;;
         esac
         done
@@ -54,21 +51,32 @@ check_option () {
 
     cd system_logs
 
+    # Condiciones para output file 'error_logs.txt'
+
+    # Si se especifica modo pero no fecha
+    # Se filtra por modo
     if [ "$m" == "true" ] && [ "$d" == "false" ]; then
         for file in *.log; do
             cat "$file" >> merged_file.log
         done
         cat merged_file.log | grep ERROR | grep "$mode"| awk '{print "Fecha:", $1, "\nHora del error:", $2, "\nDescripcion del error: ", substr($0, index($0, $5))}' > error_logs.txt
         rm merged_file.log
+
+    # Si se especifica modo y fecha
+    # Se filtra por modo y fecha
     elif [ "$m" == "true" ] && [ "$d" == "true" ]; then
         for file in *.log; do
             cat "$file" >> merged_file.log
         done
         cat merged_file.log | grep ERROR | grep "$mode" | grep "$date" | awk '{print "Fecha:", $1, "\nHora del error:", $2, "\nDescripcion del error: ", substr($0, index($0, $5))}' > error_logs.txt
         rm merged_file.log
+
+    # Si se especifica fecha pero no modo
+    # Se filtra por fecha
     elif [ "$m" == "false" ] && [ "$d" == "true" ]; then
         cat log_"$date".log | grep ERROR | awk '{print "Fecha:", $1, "\nHora del error:", $2, "\nDescripcion del error: ", substr($0, index($0, $5))}' > error_logs.txt
     fi
 }
 
+# Llama a la función principal
 main
